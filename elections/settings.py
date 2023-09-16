5f6300af5659640f3cdabddcfcb1a76336e27a1d
Chukwunazaekpere <ceo.naza.tech@gmail.com>
2023-09-16 17:25:55 +0100

    test stage

--- elections/settings.py
+++ elections/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/admin'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles/admin'),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
@@ -141,4 +141,6 @@ AUTH_USER_MODEL = "agents.Agents"
 
 ALLOWED_HOSTS = [
     'naza-bin-com-test-d87522aefc5f.herokuapp.com',
+    'localhost:8000',
+    'localhost',
 ]
